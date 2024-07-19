a8386f9d1fbc2de76c2da5fd6c9d10062dcb564d
FelipeTDR31 <felipetdrgames@gmail.com>
2024-07-19 16:52:19 -0300

    added more styles to Task.tsx

--- frontend/src/app/[user]/page.tsx
+++ frontend/src/app/[user]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Button, Drawer, IconButton, Menu, MenuItem, styled, Switch } from "@mui/material";
+import { Box, Button, Drawer, IconButton, Menu, MenuItem, Modal, styled, Switch } from "@mui/material";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import anime from 'animejs/lib/anime.es.js';
 import Image from "next/image";
@@ -144,23 +144,27 @@ export default function UserPage({ params }: { params: { user: string } }) {
       setShowDrawer(!showDrawer);
       if (!showDrawer) {
         anime({
-          targets: [".cDiv", ".main-content"],
+          targets: [".cBox", ".main-content"],
           width: "78vw",
           duration: 10,
         })
         anime({
           targets: ".main-content",
           translateX: "0vw",
+          easing: "easeOutCubic",
+          duration: 10,
         })
       }else{
         anime({
-          targets: [".cDiv", ".main-content"],
+          targets: [".cBox", ".main-content"],
           width: "100vw",
           duration: 10,
         })
         anime({
           targets: ".main-content",
           translateX: "-22vw",
+          easing: "easeOutCubic",
+          duration: 10,
         })
       }
     }
@@ -171,48 +175,39 @@ export default function UserPage({ params }: { params: { user: string } }) {
              open={showDrawer} 
              anchor="left"
              onClose={hideDrawer}
-             classes={{
-              paper: `aside-content flex flex-col justify-around p-6 font-bold w-[22vw] h-screen ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`
-             }}
-             hideBackdrop
              >
+              <Box className={`aside-content flex flex-col justify-around p-6 font-bold w-[22vw] h-screen ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
                 <h3 className={`text-3xl -ml-3 -mt-8 ${mode === "dark" ? "text-white" : "text-black"}`}>{params.user} Kanban</h3>
-                <div>
+                <Box>
                     <h4 className="text-gray-400 text-sm">Your Boards {"()"}</h4>
-                    <div className="mt-2" id="boards">
+                    <Box className="mt-2" id="boards">
                         <button className="chosen-board py-2 pl-10 pr-24 -ml-10 rounded-3xl hover:opacity-90">Default Board</button>
                         <button className="create-board py-2 pl-7 -ml-7 hover:opacity-90" onClick={() => {setShowBoardCreation(true)}}>+Create New Board</button>
-                    </div>
-                </div>
-                <div>
-                    <h4 className="text-gray-400 text-sm">Team Boards {"()"}</h4>
-                    <div className="mt-2" id="boards">
-                        <button className="chosen-board py-2 pl-10 pr-24 -ml-10 rounded-3xl hover:opacity-90">Default Board</button>
-                        <button className="create-board py-2 pl-7 -ml-7 hover:opacity-90" onClick={() => {setShowBoardCreation(true)}}>+Create New Board</button>
-                    </div>
-                </div>
-                <div>
-                    <div className={`flex gap-3 items-center size-fit rounded-md py-2 px-16 ${mode === "dark" ? "bg-primary-dark" : "bg-primary-light"}`}>
+                    </Box>
+                </Box>
+                <Box>
+                    <Box className={`flex gap-3 items-center size-fit rounded-md py-2 px-16 ${mode === "dark" ? "bg-primary-dark" : "bg-primary-light"}`}>
                         <Image src={SunImg} alt="sun" width={25} height={25} />
                         <AntSwitch checked={mode === "dark" ? true : false} onClick={toggleMode} />
                         <Image src={MoonImg} alt="moon" width={20} height={20} />
-                    </div>
-                </div>
-                <IconButton 
-                className={`font-semibold text-lg absolute bottom-[4.5rem] left-8 py-2 pr-20 pl-16 -m-16 rounded-3xl hover:opacity-80 ${mode === "dark" ? "hover:bg-gray-600" : "hover:bg-gray-300"}`}
-                onClick={hideDrawer}
-                >
-                  <button className="flex items-center gap-3" type="button"><Image src={HideImg} alt="hide" width={25} height={25} />Hide Sidebar</button>
-                </IconButton>
+                    </Box>
+                    <IconButton onClick={hideDrawer} >
+                      <Box className={`flex items-center gap-3 font-semibold py-2 pr-20 pl-16 -m-16 mt-[0.5rem] rounded-3xl hover:opacity-80 ${mode === "dark" ? "hover:bg-gray-600" : "hover:bg-gray-300"}`}>
+                        <Image src={HideImg} alt="hide" width={25} height={25} />
+                        <span className="text-lg">Hide Sidebar</span>
+                      </Box>
+                    </IconButton>
+                </Box>
+              </Box>
             </Drawer>
 
-            <IconButton onClick={hideDrawer} className="rounded-full flex justify-center items-center w-10 h-10 absolute bottom-[1rem] left-2 hover:bg-gray-600 z-10">
+            <button onClick={hideDrawer} className="rounded-full flex justify-center items-center w-10 h-10 absolute bottom-[1rem] left-2 hover:bg-gray-600 z-10">
               <Image src={SeeImg} alt="see" width={25} height={25} />
-            </IconButton>
+            </button>
 
-            <Box className={`cDiv absolute top-0 right-0 font-bold flex items-center justify-between p-5 border-b border-l border-gray-500 w-[78vw] h-[15vh] ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
+            <Box className={`cBox absolute top-0 right-0 font-bold flex items-center justify-between p-5 border-b border-l border-gray-500 w-[78vw] h-[15vh] ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
                 <h1 className={`text-xl ${mode === "dark" ? "text-white" : "text-black"}`}>Platform Launch</h1>
-                <div className="flex gap-3 items-center">
+                <Box className="flex gap-3 items-center">
                     <button className="bg-button font-bold text-base p-3 px-4 rounded-3xl" onClick={() => {setShowAddNewTask(!showAddNewTask)}}>+ Add New Task</button>
                     <Box>
                       <Button onClick={() => setShowDropdown(!showDropdown)} className="dropdown">
@@ -220,15 +215,15 @@ export default function UserPage({ params }: { params: { user: string } }) {
                       </Button>
                       <Menu
                         open={showDropdown}
+                        onClose={() => setShowDropdown(false)}
                         anchorEl={document.querySelector(".dropdown")}
-                        hideBackdrop
                       >
                         <MenuItem>Delete Board</MenuItem>
                         <MenuItem>Update Board</MenuItem>
                         <MenuItem>Delete Column</MenuItem>
                       </Menu>
                     </Box>
-                </div>
+                </Box>
             </Box>
 
             <Box className="main-content w-[78vw] absolute top-[15vh] left-[22vw] h-[80vh] pl-4 pt-4 flex gap-6">
@@ -236,59 +231,63 @@ export default function UserPage({ params }: { params: { user: string } }) {
                 <button className={`h-full w-[22vw] font-bold ${mode === "dark" ? "text-gray-500 bg-[#24242F]" : "text-gray-400 bg-[#E5E5E5]"} hover:opacity-90`}>+ New Column</button>
             </Box>
 
-          {
-            showAddNewTask ? 
-            <div className={`flex flex-col p-5 gap-4 absolute top-[5vh] left-[35vw] w-[30vw] h-[90vh] z-10 rounded-md ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
-              <h1 className={`font-semibold text-xl ${mode === "dark" ? "text-white" : "text-black"}`}>New Task</h1>
-              <form className="flex flex-col gap-3">
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="title" className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Title</label>
-                  <Input placeholder="e.g: Take a coffee" type="text" id="title" name="title" className="w-full font-semibold" />  
-                </div>
-                
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="description" className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Description</label>
-                  <textarea placeholder="e.g: Always good to take a break" name="description" id="description" rows={4} className="resize-none text-gray-400 text-sm p-2 rounded w-full input-border"></textarea>
-                </div>
-                
-                <div className="flex flex-col gap-2">
-                  <label className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Subtasks</label>
-                  <div className="flex flex-col items-center p-1 gap-2 overflow-y-auto scrollbar-hidden max-h-[5.2rem] rounded-lg bg-[rgba(0,0,0,0.2)]">
-                    {
-                      [...Array(numberOfSubtasks)].map((_, index) => (
-                        <div className="flex gap-2 subtasks" key={index} id={index.toString()}>
-                          <Input type="text" className="subtasksInput font-semibold text-sm w-[20rem]" name="subtasks" />
-                          <button type="button" className="text-gray-400 font-bold" onClick={() => {if (numberOfSubtasks > 1) document.querySelectorAll(".subtasks").forEach((subtask) => subtask.id == index.toString() ? subtask.remove() : null)}}>X</button>
-                        </div>
-                      ))
-                    }
-                  </div>
-                  <button className="bg-button font-bold text-base p-2 rounded-3xl w-full" onClick={() => setNumberOfSubtasks(numberOfSubtasks + 1)}>+ Add New Subtask</button>
-                </div>
-                
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="status" className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Status</label>
-                  <select name="status" id="status" className="w-full font-semibold input-border rounded p-1 text-gray-400">
-                    <option value="todo" className={`text-gray-400 font-semibold p-1 ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>To Do</option>
-                    <option value="inprogress" className={`text-gray-400 font-semibold p-1 ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>In Progress</option>
-                    <option value="done" className={`text-gray-400 font-semibold p-1 ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>Done</option>
-                  </select>
-                </div>
-                <button className="bg-button font-bold text-base p-2 rounded-3xl w-full">Create Task</button>
-              </form>
-            </div>
-            : null
-          }
+            <Modal
+              open={showAddNewTask}
+              onClose={() => setShowAddNewTask(false)}
+              keepMounted
+            >
+              <Box className={`flex flex-col p-5 gap-4 absolute top-[5vh] left-[35vw] w-[30vw] h-[90vh] z-10 rounded-md ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
+                <h1 className={`font-semibold text-xl ${mode === "dark" ? "text-white" : "text-black"}`}>New Task</h1>
+                <form className="flex flex-col gap-3">
+                  <Box className="flex flex-col gap-2">
+                    <label htmlFor="title" className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Title</label>
+                    <Input placeholder="e.g: Take a coffee" type="text" id="title" name="title" className="w-full font-semibold" />  
+                  </Box>
+                  
+                  <Box className="flex flex-col gap-2">
+                    <label htmlFor="description" className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Description</label>
+                    <textarea placeholder="e.g: Always good to take a break" name="description" id="description" rows={4} className="resize-none text-gray-400 text-sm p-2 rounded w-full input-border"></textarea>
+                  </Box>
+                  
+                  <Box className="flex flex-col gap-2">
+                    <label className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Subtasks</label>
+                    <Box className="flex flex-col items-center p-1 gap-2 overflow-y-auto scrollbar-hidden max-h-[5.2rem] rounded-lg bg-[rgba(0,0,0,0.2)]">
+                      {
+                        [...Array(numberOfSubtasks)].map((_, index) => (
+                          <Box className="flex gap-2 subtasks" key={index} id={index.toString()}>
+                            <Input type="text" className="subtasksInput font-semibold text-sm w-[20rem]" name="subtasks" />
+                            <button type="button" className="text-gray-400 font-bold" onClick={() => {if (numberOfSubtasks > 1) document.querySelectorAll(".subtasks").forEach((subtask) => subtask.id == index.toString() ? subtask.remove() : null)}}>X</button>
+                          </Box>
+                        ))
+                      }
+                    </Box>
+                    <button className="bg-button font-bold text-base p-2 rounded-3xl w-full" onClick={() => setNumberOfSubtasks(numberOfSubtasks + 1)}>+ Add New Subtask</button>
+                  </Box>
+                  
+                  <Box className="flex flex-col gap-2">
+                    <label htmlFor="status" className={`font-semibold text-sm ${mode === "dark" ? "text-white" : "text-black"}`}>Status</label>
+                    <select name="status" id="status" className="w-full font-semibold input-border rounded p-1 text-gray-400">
+                      <option value="todo" className={`text-gray-400 font-semibold p-1 ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>To Do</option>
+                      <option value="inprogress" className={`text-gray-400 font-semibold p-1 ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>In Progress</option>
+                      <option value="done" className={`text-gray-400 font-semibold p-1 ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>Done</option>
+                    </select>
+                  </Box>
+                  <button className="bg-button font-bold text-base p-2 rounded-3xl w-full">Create Task</button>
+                </form>
+              </Box>
+            </Modal>
 
-          {
-            showBoardCreation ? 
-            <div className={`flex flex-col items-center font-semibold p-5 gap-4 absolute top-[30vh] left-[35vw] z-10 rounded-md ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
-              <Input placeholder="Board Name" type="text" id="boardName" name="boardName" />
-              <button onClick={createBoard} className="bg-button font-bold text-base p-2 rounded-3xl w-full">Create Board</button>
-            </div>
-            :
-            null
-          }
+            <Modal
+              open={showBoardCreation}
+              onClose={() => setShowBoardCreation(false)}
+              keepMounted
+            >
+              <Box className={`flex flex-col items-center font-semibold p-5 gap-4 absolute top-[30vh] left-[35vw] z-10 rounded-md ${mode === "dark" ? "bg-secondary-dark" : "bg-secondary-light"}`}>
+                <Input placeholder="Board Name" type="text" id="boardName" name="boardName" />
+                <button onClick={createBoard} className="bg-button font-bold text-base p-2 rounded-3xl w-full">Create Board</button>
+              </Box>
+            </Modal>
+            
         </Box>
     );
 }
